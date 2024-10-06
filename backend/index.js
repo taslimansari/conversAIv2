4cf62808f553ede4a251e46d54af5f598bd4dc53
Taslim Ansari <1993taslim@gmail.com>
2024-10-06 23:16:59 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,173 @@
+import express, { text } from "express";
+import cors from "cors";
+import ImageKit from "imagekit"
+import mongoose from "mongoose";
+import UserChats from "./models/userChats.js";
+import Chat from "./models/chat.js";
+import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node'
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}))
+
+app.use(express.json())
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const imagekit = new ImageKit({
+    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
+    publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+});
+
+app.get("/api/upload", (req, res) => {
+    const result = imagekit.getAuthenticationParameters();
+    res.send(result);
+    return
+})
+
+
+app.post("/api/chats", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+    const { text } = req.body;
+
+    try {
+        // CREATE A NEW CHAT
+        const newChat = new Chat({
+            userId: userId,
+            history: [{ role: "user", parts: [{ text }] }],
+        });
+
+        const savedChat = await newChat.save();
+
+        // CHECK IF THE USERCHATS EXISTS
+        const userChats = await UserChats.find({ userId: userId });
+
+        // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+        if (!userChats.length) {
+            const newUserChats = new UserChats({
+                userId: userId,
+                chats: [
+                    {
+                        _id: savedChat._id,
+                        title: text.substring(0, 40),
+                    },
+                ],
+            });
+
+            await newUserChats.save();
+        } else {
+            // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+            await UserChats.updateOne(
+                { userId: userId },
+                {
+                    $push: {
+                        chats: {
+                            _id: savedChat._id,
+                            title: text.substring(0, 40),
+                        },
+                    },
+                },
+            );
+            res.status(201).send(newChat._id);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error creating chat!");
+    }
+});
+
+
+app.get("/api/userchats", ClerkExpressRequireAuth(),  async (req, res) => {
+    const userId = req.auth.userId;
+    try {
+        const userChats = await UserChats.find({ userId })
+        let chats = [];
+        if (userChats.length > 0) {
+            chats = userChats[0].chats;
+        }
+        res.status(200).send(chats)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send("Error fetching userChats!")
+    }
+    return
+})
+
+app.get("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+
+    try {
+        
+        if (req.params.id !== "dashboard") {
+            const chat = await Chat.findOne({ _id: req.params.id, userId });
+            if (chat) {
+                res.status(200).send(chat);
+            } else {
+                res.status(200).send({});
+            }
+        }
+        else {
+           res.status(200).send({}); 
+        }
+       
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error fetching chat!");
+    }
+    return
+});
+
+app.put("/api/chats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+    const userId = req.auth.userId;
+
+    const { question, answer, img } = req.body;
+
+    const newItems = [
+        ...(question
+            ? [{ role: "user", parts: [{ text: question }], ...(img && { img }) }]
+            : []),
+        { role: "model", parts: [{  text: answer }] },
+    ];
+
+    try {
+        const updatedChat = await Chat.updateOne(
+            { _id: req.params.id, userId },
+            {
+                $push: {
+                    history: {
+                        $each: newItems,
+                    },
+                },
+            }
+        );
+        res.status(200).send(updatedChat);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error adding conversation!");
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(401).send('Unauthenticated!')
+})
+
+
+app.listen(port, () => {
+    connect()
+    console.log(`Server running on ${port}`)
+});
---
+++ client/src/routes/dashboardPage/DashboardPage.jsx
@@ -0,0 +1,55 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import "./dashboardPage.css";
+import { useNavigate } from "react-router-dom";
+
+const DashboardPage = () => {
+  const queryClient = useQueryClient();
+
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: (text) => {
+      return fetch(`${import.meta.env.VITE_API_URL}/api/chats`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text }),
+      }).then((res) => res.json());
+    },
+    onSuccess: (id) => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ["userChats"] });
+      navigate(`/dashboard/chats/${id}`);
+    },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    if (!text) return;
+
+    mutation.mutate(text);
+  };
+  return (
+    <div className="dashboardPage">
+      <div className="texts">
+        <div className="logo">
+          <img src="/logo.png" alt="" />
+          <h1>ConversAI</h1>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="text" placeholder="Ask me anything..." />
+          <button>
+            <img src="/arrow.png" alt="" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
